3d0e1ebee623cb2fcb10918ce4d912d104c0af96
Rishit Maurya <64087730+rishitmaurya@users.noreply.github.com>
2025-02-26 13:06:25 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
+
+# test to check that the client is receiving video frames from server  
 import cv2
-
-
 cap = cv2.VideoCapture("http://"your server ip address":12346")  # Replace with server details
 while True:
     ret, frame = cap.read()
